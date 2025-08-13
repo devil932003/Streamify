b1604213570c363a81475fbc1628a7530656c021
devil932003 <142711349+devil932003@users.noreply.github.com>
2025-08-13 13:04:14 +0530

    chat-page-initiated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -41,7 +41,16 @@ if(isLoading) return <PageLoader />
           <NotificationPage />
           </Layout>) : (<Navigate to={!isAuthenticated?"/login":"/onboarding"} />)} />
         <Route path="/call" element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />} />
-        <Route path="/chat" element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />} />
+        <Route path="/chat/:id"
+         element={
+          isAuthenticated && isOnboarded?(
+            <Layout showSidebar={false}>
+              <ChatPage/>
+            </Layout>
+          ):(
+            <Navigate to={!isAuthenticated?"/login":"/onboarding"} />
+          )
+        } />
         <Route path="/onboarding" element={isAuthenticated ? (!isOnboarded ? <OnboardingPage/> : <Navigate to="/" />) : <Navigate to="/login" />} />
       </Routes>
       <Toaster/>
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,6 +1,42 @@
-import React from 'react'
+import {useParams} from "react-router";
+import { useEffect, useState } from "react";
+import useAuthUser from "../hooks/useAuthUser.js";
+import { useQuery } from "@tanstack/react-query";
+import { getStreamToken } from "../lib/api";
+import {
+  Channel,
+  ChannelHeader,
+  Chat,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
+
 
 const ChatPage = () => {
+  const {id:targetuserId} = useParams();
+  const [chatClient,setChatClient] = useState(null);
+  const [channel,setChannel] = useState(null);
+  const [loading,setLoading] = useState(true);
+  const {authUser}=useAuthUser();
+
+  const {data:tokenData} = useQuery({
+    queryKey:["streamToken"],
+    queryFn:getStreamToken,
+    enabled:!!authUser  // this will run only when authUser us available
+  })
+
+  useEffect(()=>{
+    const initChat =async()=>{
+    if(!tokenData?.tooken || !authUser) return;
+
+    try{
+      console
+    }
+    }
+  })
+
   return (
     <div>ChatPage</div>
   )
